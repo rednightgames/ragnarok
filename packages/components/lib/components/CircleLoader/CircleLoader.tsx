d00d1c10bb3de9a2046f864ce5ead671a90a7a3f
Dmitriy Chukhno <dmitr1sdadaya@protonmail.com>
2023-11-29 20:39:04 +0100

    [STYLE] Lint fix

--- packages/components/lib/components/CircleLoader/CircleLoader.tsx
+++ packages/components/lib/components/CircleLoader/CircleLoader.tsx
@@ -1,5 +1,6 @@
 import {generateUID} from "@rednight/utils";
 import {ComponentPropsWithoutRef} from "react";
+
 import {
   StyledCircle,
   StyledCircleLoader,
@@ -12,25 +13,23 @@ const CircleLoader = ({...rest}: CircleLoaderProps) => {
   const uid = generateUID("circle_loader");
 
   return (
-    <>
-      <StyledCircleLoader
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        data-testid="circle_loader"
-        {...rest}
-      >
-        <defs>
-          <circle
-            id={uid}
-            cx="8"
-            cy="8"
-            r="7"
-          />
-        </defs>
-        <StyledCircle href={`#${uid}`} />
-        <StyledLoaderCircle href={`#${uid}`} />
-      </StyledCircleLoader>
-    </>
+    <StyledCircleLoader
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      data-testid="circle_loader"
+      {...rest}
+    >
+      <defs>
+        <circle
+          id={uid}
+          cx="8"
+          cy="8"
+          r="7"
+        />
+      </defs>
+      <StyledCircle href={`#${uid}`} />
+      <StyledLoaderCircle href={`#${uid}`} />
+    </StyledCircleLoader>
   );
 };
 
