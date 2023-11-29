f2fd3fe7c52485aaf5b2b31700fbfcc6b04788e5
Dmitriy Chukhno <dmitr1sdadaya@protonmail.com>
2023-11-29 18:45:49 +0100

    [FEATURE] Utils package

---
+++ packages/utils/vite.config.ts
@@ -0,0 +1,22 @@
+import {resolve} from "path";
+import {defineConfig} from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      outDir: ["dist"],
+      exclude: ["vite.config.ts"],
+      rollupTypes: true,
+      clearPureImport: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "rednight-utils",
+      formats: ["es", "cjs"],
+    },
+    minify: "esbuild",
+  },
+});
