4e2d7841f19b6a55f08083ac67f784562633a137
Dmitriy Chukhno <dmitr1sdadaya@protonmail.com>
2024-03-23 21:20:21 +0100

    [FEATURE] Added Avatar to Header

--- applications/landing/src/app/components/Header/Header.tsx
+++ applications/landing/src/app/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import "./Header.scss";
 
-import {AppLink, Button, Logo} from "@rednight/components";
+import {AppLink, Avatar, Button, Logo} from "@rednight/components";
 import {APPS} from "@rednight/shared";
 import {HTMLAttributes} from "react";
 import {c} from "ttag";
@@ -10,23 +10,29 @@ export interface HeaderProps extends HTMLAttributes<HTMLElement> {}
 const Header = ({...restProps}: HeaderProps) => {
   return (
     <header className="header" {...restProps}>
-      <AppLink to="/" toApp={APPS.LANDING}>
-        <Logo />
-      </AppLink>
-      <div>
-        <Button shape="ghost" to="/about" toApp={APPS.LANDING} as={AppLink}>
-          {c("Header").t`About`}
-        </Button>
-        <Button shape="ghost" to="/premium" toApp={APPS.LANDING} as={AppLink}>
-          {c("Header").t`Premium`}
-        </Button>
-        <Button shape="ghost" to="/safety" toApp={APPS.LANDING} as={AppLink}>
-          {c("Header").t`Safety`}
-        </Button>
-        <Button shape="ghost" to="/support" toApp={APPS.LANDING} as={AppLink}>
-          {c("Header").t`Support`}
-        </Button>
+      <div className="section">
+        <AppLink to="/" toApp={APPS.LANDING}>
+          <Logo />
+        </AppLink>
+        <div className="links">
+          <Button shape="ghost" to="/about" toApp={APPS.LANDING} as={AppLink}>
+            {c("Header").t`About`}
+          </Button>
+          <Button shape="ghost" to="/premium" toApp={APPS.LANDING} as={AppLink}>
+            {c("Header").t`Premium`}
+          </Button>
+          <Button shape="ghost" to="/safety" toApp={APPS.LANDING} as={AppLink}>
+            {c("Header").t`Safety`}
+          </Button>
+          <Button shape="ghost" to="/support" toApp={APPS.LANDING} as={AppLink}>
+            {c("Header").t`Support`}
+          </Button>
+        </div>
       </div>
+      <Avatar
+        src="https://cdn.rednightgames.com/avatars/153435384675569664"
+        size="small"
+      />
     </header>
   );
 };
